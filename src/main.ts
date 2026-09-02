ddb8a6b5cbeeac6c829752df57a44cc0e35af2c4
Török Donát <torok.donat07@gmail.com>
2026-09-02 14:03:50 +0200

    data read

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import './style.css';
+
+const URL_LINK: string = "https://retoolapi.dev/4vkBhL/data";
+let data;
+
+function printData() {
+  const table:HTMLTableElement = (document.getElementById("table") as HTMLTableElement);
+  table.innerHTML = "";
+
+  const row:HTMLTableRowElement = document.createElement("tr");
+
+
+}
+
+async function loadData() {
+  const response:Response = await fetch(URL_LINK);
+  data = await JSON.parse(await response.text());
+
+  printData();
+};
+
+function init() {
+  loadData();
+}
+
+document.addEventListener("DOMContentLoaded", init);
